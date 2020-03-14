522426f91c5bd25ec6c56fbb685be6d2821e94e8
Maciej Uberna <maciej.uberna@gmail.com>
2020-03-14 20:20:15 +0100

    small fix

--- redux-first/src/Store/reducer.js
+++ redux-first/src/Store/reducer.js
@@ -6,8 +6,8 @@ const reducer = (state = initialState, action) => {
     if(action.type === 'INCREMENT') {
         return {
             counter: state.counter + 1
-        }
-    }
+        };
+    };
     return state;
 };
 
