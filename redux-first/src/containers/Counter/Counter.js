28884d13deedcdd88134d45bf93a172d3ece07ff
Maciej Uberna <maciej.uberna@gmail.com>
2020-03-14 21:20:21 +0100

    Assigment4 Students' solution I particulary like.

--- redux-first/src/containers/Counter/Counter.js
+++ redux-first/src/containers/Counter/Counter.js
@@ -1,38 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
-class Counter extends Component {
-
-    render () {
-        return (
-            <div>
-                <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add 5" clicked={this.props.onAddCounter.bind(this,5)}  />
-                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter.bind(this,5)}  />
-            </div>
-        );
-    };
-
+const controls = {
+    inc: {
+        label: 'Increment',
+        action: 'INCREMENT',
+    },
+    dec: {
+        label: 'Decrement',
+        action: 'DECREMENT',
+    },
+    add: {
+        label: 'Add 5',
+        action: 'ADD',
+        value: 5,
+    },
+    sub: {
+        label: 'Subtract 5',
+        action: 'SUBTRACT',
+        value: 5,
+    }
 }
-
-const mapStateToProps = state => {
-    return {
-        ctr: state.counter
-    };
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
-        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
-        onAddCounter: (value) => dispatch({type: 'ADD', value: value}),
-        onSubtractCounter: (value) => dispatch({type: 'SUBTRACT', value: value})
-    };
-};
-
+ 
+const Counter = props => (
+    <div>
+        <CounterOutput value={props.ctr} />
+        {Object.keys(controls).map(key => (
+            <CounterControl
+                key={key}
+                label={controls[key].label}
+                clicked={props.onModifyCounter.bind(this,controls[key].action, controls[key].value)} />
+        ))}
+    </div>
+);
+ 
+const mapStateToProps = state => ({ ctr: state.counter });
+ 
+const mapDispatchToProps = dispatch => ({
+    onModifyCounter: (type, value) => dispatch({ type, value })
+});
+ 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
