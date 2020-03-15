c39d9b3cbf20718e52594c1b870fe35c71180581
Maciej Uberna <maciej.uberna@gmail.com>
2020-03-15 19:49:48 +0100

    minor adjustments of case statement in reducer.

--- redux-first/src/Store/reducer.js
+++ redux-first/src/Store/reducer.js
@@ -5,26 +5,26 @@ const initialState = {
 const reducer = (state = initialState, action) => {
 
     switch(action.type) {
-        case('INCREMENT'):
+        case 'INCREMENT':
             return {
                 counter: state.counter + 1
             };
-        case('DECREMENT'):
+        case 'DECREMENT':
             return {
                 counter: state.counter - 1
             }
-        case('ADD'):
+        case 'ADD':
             return {
                 counter: state.counter + action.value
             }
-        case('SUBTRACT'):
+        case 'SUBTRACT':
             return {
                 counter: state.counter - action.value
             }
         default:
             return state;
     };
-    
+
 };
 
 export default reducer;
