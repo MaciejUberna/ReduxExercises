684cd2bd2da332b72d589a414de1cc077576dc0f
Maciej Uberna <maciej.uberna@gmail.com>
2020-03-18 20:14:41 +0100

    Installed redux and react-redux on assigment2

--- assignment2/src/containers/Persons.js
+++ assignment2/src/containers/Persons.js
@@ -3,6 +3,30 @@ import React, { Component } from 'react';
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
 
+const persons =
+    [
+        'Max',
+        'Maciek',
+        'Marek',
+        'Ollin',
+        'Rysiek',
+        'Ola',
+        'Monika',
+        'Anna',
+        'Kasia',
+        'Zeus',
+        'Ares',
+        'Apollo',
+        'Poseidon',
+        'Atena',
+        'Łukasz',
+        'Daniel',
+        'Aleksandra',
+        'Solidariusz',
+        'Irena',
+        'Paździoch'
+    ];
+
 class Persons extends Component {
     state = {
         persons: []
@@ -11,8 +35,8 @@ class Persons extends Component {
     personAddedHandler = () => {
         const newPerson = {
             id: Math.random(), // not really unique but good enough here!
-            name: 'Max',
-            age: Math.floor( Math.random() * 40 )
+            name: persons[Math.floor(Math.random()*20)],
+            age: Math.ceil( Math.random() * 40 )
         }
         this.setState( ( prevState ) => {
             return { persons: prevState.persons.concat(newPerson)}
@@ -20,7 +44,7 @@ class Persons extends Component {
     }
 
     personDeletedHandler = (personId) => {
-        this.setState( ( prevState ) => {
+        this.setState( ( prevState ) => { 
             return { persons: prevState.persons.filter(person => person.id !== personId)}
         } );
     }
@@ -33,7 +57,7 @@ class Persons extends Component {
                     <Person 
                         key={person.id}
                         name={person.name} 
-                        age={person.age} 
+                        age={person.age}
                         clicked={() => this.personDeletedHandler(person.id)}/>
                 ))}
             </div>
